4b4dfa7fac1e34ab04f4bf1f4823b797ad77e766
Jean-Luc Pons <pons@esrf.fr>
2019-03-16 10:57:07 +0100

    Removed debugging code

--- main.cpp
+++ main.cpp
@@ -111,17 +111,6 @@ void parseFile(string fileName, vector<string> &lines) {
 
 }
 
-/*
-beta = 0x7ae96a2b657c07106e64479eac3434e99cf0497512f58995c1396c28719501ee # (beta ^ 3 = 1 mod p)
-beta2 = 0x851695d49a83f8ef919bb86153cbcb16630fb68aed0a766a3ec693d68e6afa40 # (beta ^ 2)
-
-lambda = 0x5363ad4cc05c30e0a5261c028812645a122e22ea20816678df02967c1b23bd72 # (lambda ^ 3 = 1 mod n)
-lambd2 = 0xac9c52b33fa3cf1f5ad9e3fd77ed9ba4a880b9fc8ec739c2e0cfc810b51283ce #(lambda ^ 2)
-
-if (x, y) = k * G, then(beta*x, y) = lambda * k*G and
-(beta2*x, y) = lambda2 * k*G
-*/
-
 // ------------------------------------------------------------------------------------------
 
 int main(int argc, char* argv[]) {
@@ -133,75 +122,6 @@ int main(int argc, char* argv[]) {
   // Init SecpK1
   Secp256K1 secp;
   secp.Init();
-
-#if 0
-  // Test
-  //Int::SetupField(&secp.order);
-  //printf("R2 = %s\n",Int::GetR2()->GetBase16().c_str());
-  
-  Int beta;
-  Int lambda;
-  beta.SetBase16("7ae96a2b657c07106e64479eac3434e99cf0497512f58995c1396c28719501ee");
-  lambda.SetBase16("5363ad4cc05c30e0a5261c028812645a122e22ea20816678df02967c1b23bd72");
-  Int beta2;
-  Int lambda2;
-  beta2.SetBase16("851695d49a83f8ef919bb86153cbcb16630fb68aed0a766a3ec693d68e6afa40");
-  lambda2.SetBase16("ac9c52b33fa3cf1f5ad9e3fd77ed9ba4a880b9fc8ec739c2e0cfc810b51283ce");
-
-  bool wrong = false;
-  for (int i = 0; i < 1 && !wrong; i++) {
-
-    Int k;
-    Int kn;
-    k.Rand(256);
-    kn.Set(&k);
-    kn.Neg();
-    kn.Add(&secp.order);
-    Point p = secp.ComputePublicKey(&k);
-    Point pn = secp.ComputePublicKey(&kn);
-    p.x.ModMulK1(&beta);
-    pn.x.ModMulK1(&beta);
-
-    k.ModMulK1order(&lambda);
-    kn.ModMulK1order(&lambda);
-    Point p2 = secp.ComputePublicKey(&k);
-    Point p2n = secp.ComputePublicKey(&kn);
-
-    if (!p.equals(p2)) {
-      wrong = true;
-      printf("Wrong #1\n");
-      printf("(beta*x, y)=\n%s\n", p.toString().c_str());
-      printf("(beta*x, y)=\n%s\n", p2.toString().c_str());
-    }
-
-    if (!p.equals(p2)) {
-      wrong = true;
-      printf("Wrong #1\n");
-      printf("-(beta*x, y)=\n%s\n", pn.toString().c_str());
-      printf("-(beta*x, y)=\n%s\n", p2n.toString().c_str());
-    }
-
-    k.Rand(256);
-    p = secp.ComputePublicKey(&k);
-    p.x.ModMulK1(&beta2);
-
-
-    k.ModMulK1order(&lambda2);
-    p2 = secp.ComputePublicKey(&k);
-    if (!p.equals(p2)) {
-      wrong = true;
-      printf("Wrong #2\n");
-      printf("(beta2*x, y)=\n%s\n", p.toString().c_str());
-      printf("(beta2*x, y)=\n%s\n", p2.toString().c_str());
-    }
-
-  }
-
-  printf("OK!\n");
-  exit(0);
-
-#endif
-
 
   // Browse arguments
   if (argc < 2) {
